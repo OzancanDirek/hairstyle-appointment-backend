0aa8b6a46c1887414f8e92ad510b8ba7a1c55eca
ozancandirek <ozan_direk@hotmail.com>
2025-10-27 01:56:39 +0300

    Railway URL CORS'a eklendi

--- src/main/java/com/example/randevutakip/Config/SecurityConfig.java
+++ src/main/java/com/example/randevutakip/Config/SecurityConfig.java
@@ -52,7 +52,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://hairstyle-appointment-web-production.up.railway.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
